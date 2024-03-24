c91683a8905f034be9313730da8b1e7c5c09fa76
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-03-24 16:32:18 +0000

    Se rectifica el margen

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron =() => {
     return (
-      <div className="container">
+      <div className="container mt-4">
         <div className="position-relative p-5 text-start text-muted jumboStyle border border-dashed rounded-2">
           <h1 className="text-body-emphasis text-black">A Warm Welcome!</h1>
           <p className="col mb-4 text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint vel quibusdam corrupti perspiciatis vitae officia. Impedit, minima distinctio nemo repudiandae cum dignissimos fugit maxime, porro hic corporis tempore eum nobis.</p>
