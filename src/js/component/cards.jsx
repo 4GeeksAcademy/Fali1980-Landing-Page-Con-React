a60f63860ab7a9a841c96c5cc6b767c9c660d3f8
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-03-24 16:28:26 +0000

    Primer commit

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Cards =()=>{
+   return (
+    <div className="container my-4">
+        <div className="row row-gap-3 g-4">
+            <div className="col-sm-12 col-md-6 col-lg-3">
+                <div className="card h-100">
+                    <div className="imageCard d-flex justify-content-center align-items-center fs-2 text-secondary">500 x 325</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repellendus quasi temporibus amet nulla, saepe nihil inventore explicabo velit doloribus!</p>
+                    </div>
+                    <div className="card-footer text-center">
+                        <button className="btn btn-primary px-4" type="button">
+                            Find out more!
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-3">
+                <div className="card h-100">
+                    <div className="imageCard d-flex justify-content-center align-items-center fs-2 text-secondary">500 x 325</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repellendus quasi temporibus amet nulla, saepe nihil inventore explicabo velit doloribus!</p>
+                    </div>
+                    <div className="card-footer text-center">
+                        <button className="btn btn-primary px-4" type="button">
+                            Find out more!
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-3">
+                <div className="card h-100">
+                    <div className="imageCard d-flex justify-content-center align-items-center fs-2 text-secondary">500 x 325</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repellendus quasi temporibus amet nulla, saepe nihil inventore explicabo velit doloribus!</p>
+                    </div>
+                    <div className="card-footer text-center">
+                        <button className="btn btn-primary px-4" type="button">
+                            Find out more!
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-12 col-md-6 col-lg-3">
+                <div className="card h-100">
+                    <div className="imageCard d-flex justify-content-center align-items-center fs-2 text-secondary">500 x 325</div>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam repellendus quasi temporibus amet nulla, saepe nihil inventore explicabo velit doloribus!</p>
+                    </div>
+                    <div className="card-footer text-center">
+                        <button className="btn btn-primary px-4" type="button">
+                            Find out more!
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+
+   );
+}
+
+export default Cards
