e18781a77e47849b99eb4e2844619a1e14012805
Rafael <154633798+Fali1980@users.noreply.github.com>
2024-03-24 17:05:05 +0000

    Se rectifica el botton toggler

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Navbar =()=>{
    return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body" data-bs-theme="dark">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary bg-dark border-bottom border-body fixed-top" data-bs-theme="dark">
         <div className="container">
             <a className="navbar-brand text-white fw-semibold" href="#">Start Bootstrap</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+            <button className="navbar-toggler buttonToggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon barsHor"></span>
             </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
